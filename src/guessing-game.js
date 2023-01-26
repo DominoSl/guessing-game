621a28fc80c8dd80ed02909ff7783667865753b9
DominoSL <artemenko.mik@gmail.com>
2023-01-26 15:53:22 +0200

    feat: solution to the problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(
+      memo = undefined,
+      target = undefined,
+    ) {}
 
     setRange(min, max) {
-
+      this.memo = Array.from({ length: max - min }, (_, idx) => min + idx);
     }
 
     guess() {
+      this.target = this.memo[Math.round(this.memo.length / 2)]
 
+      return this.target;
     }
 
     lower() {
-
+      this.memo = this.memo.slice(0, this.memo.indexOf(this.target))
     }
 
     greater() {
-
+      this.memo = this.memo.slice(this.memo.indexOf(this.target))
     }
 }
 
