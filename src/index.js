2e7e1ee3b31313cc4ba05fd013f850ae005d3f4f
dominosl <artemenko.mik@gmail.com>
2025-08-28 07:23:12 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 class GuessingGame {
-  constructor() {
-    throw new Error('Not implemented');
+  setRange(min, max) {
+    this.memo = Array.from({ length: max - min }, (_, i) => min + i);
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  guess() {
+    this.target = this.memo[Math.round(this.memo.length / 2)];
 
-  lower() { }
+    return this.target;
+  }
 
-  greater() { }
-*/
+  lower() {
+    const targetIndex = this.memo.indexOf(this.target);
+
+    this.memo = this.memo.slice(0, targetIndex);
+  }
+
+  greater() {
+    const targetIndex = this.memo.indexOf(this.target);
+
+    this.memo = this.memo.slice(targetIndex);
+  }
 }
 
 module.exports = GuessingGame;
